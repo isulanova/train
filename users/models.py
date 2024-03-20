d0a36a49fa552ebbc3118fd3d3d7bf504794f50c
isulanova <127166784+isulanova@users.noreply.github.com>
2024-03-20 17:26:50 +0300

    dogs site 2 commit

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Аватар')
+
+    class Meta:
+        db_table = 'user'
+
+    def __str__(self):
+        return self.username
---
+++ users/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('user:login'))
+    else:
+        form = UserRegistrationForm()
+    
+    context = {
+        'title': 'Home - Регистрация',
+        'form': form
+    }
+    return render(request, 'users/registration.html', context)
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+     
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/profile.html', context)
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('main:index'))
+
